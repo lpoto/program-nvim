b06baaf98032fea48218da635ba6cb876749e252
potocnikluka <luka.potochnik@gmail.com>
2021-12-18 12:35:14 +0100

    :u for upper in expand string

--- lua/program/utils.lua
+++ lua/program/utils.lua
@@ -13,6 +13,10 @@ function M.expand(args)
 					string.sub(v2, 1, 3) == '%:t' or
 					string.sub(v2, 1, 3) == '%:r' or
 					string.sub(v2, 1, 3) == '%:e'then
+                    if string.sub(v2, -2) ==  ":u" then
+                        parts[k2] = string.upper(
+                        vim.fn.expand(string.sub(v2, 1, -3)))
+                    end
 					parts[k2] = vim.fn.expand(v2)
 				else
 					parts[k2] = v2
